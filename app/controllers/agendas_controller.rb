fd1fb227a339e0dc5bdfd82748a9c28add2139ff
teramoto-chihiro <komorikiri1031@gmail.com>
2021-06-15 06:45:13 +0000

    修正

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,26 +1,23 @@
 class AgendasController < ApplicationController
   # before_action :set_agenda, only: %i[show edit update destroy]
-
+  before_action :set_agenda, only: %i[destroy]
   def index
     @agendas = Agenda.all
   end
-
   def new
     @team = Team.friendly.find(params[:team_id])
     @agenda = Agenda.new
   end
-
   def create
     @agenda = current_user.agendas.build(title: params[:title])
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-    redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
   end
-
   def destroy
     if current_user == @agenda.user || current_user == @agenda.team.owner
       @agenda.destroy
@@ -30,13 +27,10 @@ class AgendasController < ApplicationController
       redirect_to dashboard_url, notice: I18n.t('views.messages.cannot_delete_agenda')
     end
   end
-
   private
-
   def set_agenda
     @agenda = Agenda.find(params[:id])
   end
-
   def agenda_params
     params.fetch(:agenda, {}).permit %i[title description]
   end
